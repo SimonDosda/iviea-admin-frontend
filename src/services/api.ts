f88776f9b5e1cb91aaf5a0a54d6e3b9f596d6158
Simon Dosda <sdosda@wiremind.fr>
2021-05-07 21:05:06 +0200

    feat: manage auth for printful

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,24 @@
 import axios from "axios";
+import { getUser } from "./auth";
 
 const api = axios.create({
   baseURL: `${process.env.GATSBY_API_URL}`,
   headers: { "Content-Type": "application/json; charset=utf-8" },
 });
+
+export async function getWithToken<T>(url: string): Promise<T> {
+  const user = getUser();
+  if (!user) {
+    return null;
+  }
+  const { status, data } = await api.get<T>(url, {
+    headers: { Authorization: `Bearer ${user.token}` },
+  });
+  if (status !== 200) {
+    console.log(data);
+    return null;
+  }
+  return data;
+}
 
 export default api;
--- src/services/printful.ts
+++ src/services/printful.ts
@@ -4,34 +4,18 @@ import {
   PrintfulVariantT,
 } from "../models/printful";
 import { ProductT, VariantT } from "../models/product";
-import api from "./api";
+import { getWithToken } from "./api";
 
 export async function getProducts(): Promise<PrintfulProductT[]> {
-  const { status, data } = await api.get<PrintfulProductT[]>(
-    "/printful/products",
-  );
-  if (status !== 200) {
-    console.error(data);
-    return [];
-  }
-  return data;
+  return await getWithToken("/printful/products");
 }
 
 export async function getProduct(productId: number): Promise<ProductT> {
-  const { status, data } = await api.get<PrintfulProductWithVariants>(
-    `/printful/products/${productId}`,
-  );
-  if (status !== 200) {
-    console.error(data);
-    return null;
-  }
-  return parseProduct(data);
+  const data = await getWithToken<PrintfulProductWithVariants>(`/printful/products/${productId}`);
+  return data ? parseProduct(data) : null;
 }
 
-function parseProduct({
-  sync_product,
-  sync_variants,
-}: PrintfulProductWithVariants): ProductT {
+function parseProduct({ sync_product, sync_variants }: PrintfulProductWithVariants): ProductT {
   return {
     id: sync_product.id,
     name: sync_product.name,
@@ -47,12 +31,7 @@ function parseProduct({
   };
 }
 
-function parseVariant({
-  id,
-  name,
-  retail_price,
-  sku,
-}: PrintfulVariantT): VariantT {
+function parseVariant({ id, name, retail_price, sku }: PrintfulVariantT): VariantT {
   return {
     id,
     name,
